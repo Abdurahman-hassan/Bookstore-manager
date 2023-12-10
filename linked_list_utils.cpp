ff7835738d396b35a1401eae858c20eb207d3ee8
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-12-10 04:34:32 +0200

    Adding linked_list_utils.cpp and linkedlist_header.h files that includes the generic and core class with constractor

---
+++ linked_list_utils.cpp
@@ -0,0 +1,8 @@
+# include "linkedlist_header.h"
+
+template <class L>
+List<L>::List() {
+    head = NULL;
+    cursor = NULL;
+    prev = NULL;
+}
---
+++ linkedlist_header.h
@@ -0,0 +1,25 @@
+#ifndef LINKEDLIST_HEADER_H
+#define LINKEDLIST_HEADER_H
+
+# include <iostream>
+using namespace std;
+using std::string;
+
+template <class L>
+class List
+{
+public:
+    List();
+
+private:
+    class Node
+    {
+    public:
+        int key;
+        L data;
+        Node *next;
+    };
+    Node *head, *cursor, *prev;
+};
+
+#endif //LINKEDLIST_HEADER_H
